6c21363e12319373a766601c7e26cb1b0d9b6d53
Volodymyr Liapota <rimidalvatopayl@gmail.com>
2025-12-25 18:09:16 +0200

    add custom js deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,21 @@
 const core = require("@actions/core");
-const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 function run() {
+  try {
+    throw new Error("Sorry, but i do not have s3 set up for all of this");
+
+    const bucket = core.getInput("bucket", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true });
+    const distFolder = core.getInput("dist-folder", { required: true });
+
+    const s3Uri = `s3://${bucket}`;
+
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  } catch (error) {
+    exec.exec(`echo "${error.message || "Something gone wrong :("}"`);
+  }
+
   core.notice("Hello from JS action - THATS WOWWWW");
 }
 
