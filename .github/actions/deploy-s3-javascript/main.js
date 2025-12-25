523a39e9334f8ce1cb9ee68d96399978ed5f96a0
Volodymyr Liapota <rimidalvatopayl@gmail.com>
2025-12-25 18:16:56 +0200

    add output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -12,10 +12,12 @@ function run() {
     const s3Uri = `s3://${bucket}`;
 
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    const url = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
   } catch (error) {
     exec.exec(`echo "${error.message || "Something gone wrong :("}"`);
   }
-
+  core.setOutput("website-url", "http://my-website-url");
   core.notice("Hello from JS action - THATS WOWWWW");
 }
 
